8bb22159ba40b29d4a4a71614dfe29bec15e2757
tahseenzahra <tahseen1zahra@gmail.com>
2022-10-05 17:27:49 +0500

    Resolve linters

--- src/modules/addndel.test.js
+++ src/modules/addndel.test.js
@@ -1,10 +1,10 @@
-const mockTasks = require ('../__mock__/localStorage.js');
-const mocktest = require ('./mocktask.js');
+const mockTasks = require('../__mock__/localStorage.js');
+const mocktest = require('./mocktask.js');
+
 const testlist = new mocktest('task1', 1, false);
 const testlist2 = new mockTest('task2', 2, false);
 
-test('add items to list test', () => {   
-expect(mocktest.addList(testlist.getItems()).length).toBe(mockTasks.length)
-expect(mocktest.addList(testlist2.getItems()).length).toBe(mockTasks.length)
+test('add items to list test', () => {
+  expect(mocktest.addList(testlist.getItems()).length).toBe(mockTasks.length);
+  expect(mocktest.addList(testlist2.getItems()).length).toBe(mockTasks.length);
 });
-
--- src/modules/mocktask.js
+++ src/modules/mocktask.js
@@ -1,30 +1,30 @@
-const mockTasks = require ('../__mock__/localStorage.js');
+const mockTasks = require('../__mock__/localStorage.js');
 
 class mockTest {
-    constructor (description, index, complete ) {
-        this.description = description;
-        this.index = index;
-        this.complete = complete;
-    }
+  constructor(description, index, complete) {
+    this.description = description;
+    this.index = index;
+    this.complete = complete;
+  }
 
-    addList(items) {
-       mockTasks.push(items);
-       return mockTasks; 
-    }
+  addList(items) {
+    mockTasks.push(items);
+    return mockTasks;
+  }
 
-    deleteList(index) {
-        mockTasks.splice(index, 1);
-        return mockTasks; 
-    }
+  deleteList(index) {
+    mockTasks.splice(index, 1);
+    return mockTasks;
+  }
 
-    getItems() {
-        const task = {
-            description: this.description,
-            index: this.index,
-            complete: this.complete,
-        }
-        return task;
-    }
+  getItems() {
+    const task = {
+      description: this.description,
+      index: this.index,
+      complete: this.complete,
+    };
+    return task;
+  }
 }
 
 module.export = mockTest;
