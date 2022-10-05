b1af3399d8f8283a9378d1ebd09ef2ea07314f51
Iptysam <mukadamiptisam@gmail.com>
2022-10-06 00:38:19 +0300

    Add describe() method

--- src/modules/addndel.test.js
+++ src/modules/addndel.test.js
@@ -4,12 +4,16 @@ const Mocktest = require('./mocktask.js');
 const testlist = new Mocktest('task1', 1, false);
 const testlist2 = new Mocktest('task2', 2, false);
 
+describe('adding items', () => {
 test('add items to list test', () => {
   expect(Mocktest.addList(testlist.getItems()).length).toBe(mockTasks.length);
   expect(Mocktest.addList(testlist2.getItems()).length).toBe(mockTasks.length);
 });
+});
 
+describe('deleting items', () => {
 test('delete items to list test', () => {
   expect(Mocktest.deleteList(testlist.getItems()).length).toBe(mockTasks.length);
   expect(Mocktest.deleteList(testlist2.getItems()).length).toBe(mockTasks.length);
+});
 });
