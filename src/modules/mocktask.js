71d9d6be5a94024ea2ae8725c40f5176d3816a40
Iptysam <mukadamiptisam@gmail.com>
2022-10-05 13:50:24 +0300

    Update files

---
+++ src/modules/mocktask.js
@@ -0,0 +1,30 @@
+const mockTasks = require ('../__mock__/localStorage.js');
+
+class mockTest {
+    constructor (description, index, complete ) {
+        this.description = description;
+        this.index = index;
+        this.complete = complete;
+    }
+
+    addList(items) {
+       mockTasks.push(items);
+       return mockTasks; 
+    }
+
+    deleteList(index) {
+        mockTasks.splice(index, 1);
+        return mockTasks; 
+    }
+
+    getItems() {
+        const task = {
+            description: this.description,
+            index: this.index,
+            complete: this.complete,
+        }
+        return task;
+    }
+}
+
+module.export = mockTest;
